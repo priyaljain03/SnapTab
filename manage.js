dfd3a7c77b8b8103d50082934909a8f6ebae959e
priyaljain03 <jainpriyal03@gmail.com>
2022-01-17 03:44:59 +0530

    Changes in UI

--- manage.js
+++ manage.js
@@ -1,28 +1,40 @@
 console.log("Manage file loading")
-function showItems (){
-  chrome.storage.local.get("Alltabs",(items)=>{
+
+
+function showItems() {
+  chrome.storage.local.get("Alltabs", (items) => {
+
     let listOfItems = items["Alltabs"];
     let html = ``
     let counter = 0
-    listOfItems.forEach((item,index)=>{
-        html+= `
-        <div class="col ">
+    listOfItems.forEach((item, index) => {
+
+      html += `
+        <div class="col my-5">
         <div class="card-container">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <div class="card-header sticky-top">
-                <button type="button" class="btn-close btn-close-white btn-close-sm delete" id="${index} aria-label="Close"></button>
-                <h5 class="card-title m-0 p-0" style="font-size:13px;">${item.timestamp}</h5>
-              </div> 
-              ${counter = 0}   
-              ${item.Tabs.map(tab=>{
-                return (`<li class="card-subtitle mb-2 mt-2 text-muted"><a href="${tab}">${tab}</a></li>`)
-              }).join("")
-            }   
+          <div class="card"">
+          <div class="card-header sticky-top">
+          <button type="button" class="btn-close btn-close-white btn-close-sm delete" id="${index} aria-label="Close"></button>
+          <h5 class="card-title m-0 p-0" style="font-size:13px;">${item.date} | ${item.time}</h5>
+        </div> 
+            <div class="card-body mb-4">
+             
+  
+                  ${item.Tabs.map(tab => {
+        return (`<li class="card-subtitle mb-2 mt-2 text-muted"><a href="${tab}">${tab}</a></li>`)
+      }).join("")
+        }   
+            
             </div>
+            <div class="card-footer footer text-muted">
+            <hr />
+        </div>
           </div>    
-          <div><h6 class="btn btn-sm mt-3 openAll" id="${index}">Open All</h6></div>
-        </div>   
+          
+        </div> 
+       
+        <div><span class="btn btn-outline mt-2 openAll" id="${index}">Open all</span></div>
+        
       </div>
    `
     })
@@ -30,42 +42,42 @@ function showItems (){
     document.getElementById("row").innerHTML = html
     deleteNode()
     openAll(listOfItems)
-})
+  })
 }
 
 
 
-function deleteNode (){
+function deleteNode() {
   let deleteBtns = [...document.getElementsByClassName('delete')]
-  deleteBtns.forEach(btn =>btn.addEventListener('click',()=>{
+  deleteBtns.forEach(btn => btn.addEventListener('click', () => {
     console.log(btn)
-    chrome.storage.local.get("Alltabs",(items)=>{
+    chrome.storage.local.get("Alltabs", (items) => {
       let allItems = items["Alltabs"]
       console.log(btn)
-      allItems.splice(btn.id,1)
-      chrome.storage.local.set({"Alltabs":allItems},()=>{
-        console.log("This is result")
+      allItems.splice(btn.id, 1)
+      chrome.storage.local.set({ "Alltabs": allItems }, () => {
+
         showItems()
       })
-      
+
     })
   })
-)
+  )
 }
 
 
-function openAll(Alltabs){
+function openAll(Alltabs) {
   console.log("Opened all tabs")
   let openAllBtns = [...document.getElementsByClassName("openAll")]
-  openAllBtns.forEach(item=>item.addEventListener('click',(e)=>{
+  openAllBtns.forEach(item => item.addEventListener('click', (e) => {
     console.log(e.target)
     let tabs = Alltabs[e.target.id].Tabs
-    tabs.forEach((tab)=>{
-      chrome.tabs.create({url:tab})
+    tabs.forEach((tab) => {
+      chrome.tabs.create({ url: tab })
     })
     console.log(tabs)
   }))
-  
+
 }
 
 showItems()
--- popup.js
+++ popup.js
@@ -1,42 +1,42 @@
-document.getElementById("snapTab").addEventListener('click',(e)=>{
-    console.log(e)
+document.getElementById("snapTab").addEventListener('click', (e) => {
     let tabs = []
-    chrome.windows.getAll({populate:true}).then((data)=>{
-        data.forEach((item)=>{
-            item.tabs.forEach((tab)=>{
+    chrome.windows.getAll({ populate: true }).then((data) => {
+        data.forEach((item) => {
+            item.tabs.forEach((tab) => {
                 tabs.push(tab.url)
             })
         })
         let today = new Date()
-        let date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
-        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
-        let dateTimeNow = date + " " + time
-        
-        let obj ={
-            timestamp:dateTimeNow,
-            Tabs : tabs
-        } 
+        let date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear()
+        let time = today.getHours() + ":" + today.getMinutes() + (today.getHours() > 0 && today.getHours() < 12 ? 'AM' : 'PM')
+        // let dateTimeNow = date + " " + time
 
-        chrome.storage.local.get("Alltabs",(items)=>{
+        let obj = {
+            time: time,
+            date: date,
+            Tabs: tabs
+        }
+
+        chrome.storage.local.get("Alltabs", (items) => {
             console.log(items)
-            let listOfItems ;
-            if(JSON.stringify(items) == '{}'){
+            let listOfItems;
+            if (JSON.stringify(items) == '{}') {
                 console.log("Here")
                 listOfItems = []
-            }else{
+            } else {
                 console.log("Here2")
                 listOfItems = items["Alltabs"]
             }
             listOfItems.unshift(obj)
-            chrome.storage.local.set({"Alltabs":listOfItems},()=>{   
+            chrome.storage.local.set({ "Alltabs": listOfItems }, () => {
                 e.target.innerHTML = "Snapped"
                 // window.close()  
             })
-        }) 
+        })
     })
 })
 
 
-document.getElementById("manageBtn").addEventListener('click',()=>{
-    chrome.runtime.openOptionsPage() 
+document.getElementById("manageBtn").addEventListener('click', () => {
+    chrome.runtime.openOptionsPage()
 })
